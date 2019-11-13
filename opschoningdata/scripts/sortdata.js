2aece7ec1a3ec67ccbc6c615ffd25876cb039289
Ramon96 <rfm1@live.nl>
2019-11-13 14:01:49 +0100

    cleaned up some code

--- opschoningdata/scripts/sortdata.js
+++ opschoningdata/scripts/sortdata.js
@@ -1,9 +1,6 @@
 import {data} from '../input/input.js';
-let newDataArray = [];
 
-// console.log(stringToArray(data))
-// console.log(fixCoordinates(stringToArray(data)));
-fixCoordinates(stringToArray(data));
+console.log(fixCoordinates(stringToArray(data)));
 
 function stringToArray(string){
     return  string.split('\n');
@@ -11,32 +8,10 @@ function stringToArray(string){
 
 //deze functie zorgt voor het opschonen van de vuile data
 function fixCoordinates(geo){
-  /*for(let item of geo){
-    if(!isNumber(item)){
-      if(item.startsWith("(")){
-        //dit haalt de haakjes weg van de item
-        newDataArray.push(item.replace(/[()]/g, ""));
-      }
-    }
-    if(item.includes("°")){
-      // Als je in deze if statement komt dan is het item in DMS formaat genoteerd
-      let dms = item.split(/[^\d\w\.]+/);
-      let lat = convertDMS(dms[0], dms[1], dms[2], dms[3]);
-      let long = convertDMS(dms[4], dms[5], dms[6], dms[7]);
-      // Door de kracht van javascript word door latlong een string omdat de komma ook als een string geconcat word
-      const latlong = lat + "," +  long;
-      newDataArray.push(latlong);
-    }
-    if(isNumber(item) && !item.includes("°")){
-      // als het niet in dms formaat is en als het een getal is dan is hij gewoon goed.
-      newDataArray.push(item);
-    }
-  }*/
   const newData = geo.map(item => {
     if(!isNumber(item)){
       if(item.startsWith("(")){
         //dit haalt de haakjes weg van de item
-        // newDataArray.push(item.replace(/[()]/g, ""));
         return item.replace(/[()]/g, "");
       }
       }
@@ -47,16 +22,15 @@ function fixCoordinates(geo){
         let long = convertDMS(dms[4], dms[5], dms[6], dms[7]);
         // Door de kracht van javascript word door latlong een string omdat de komma ook als een string geconcat word
         const latlong = lat + "," +  long;
-        // newDataArray.push(latlong);
         return latlong;
       }
       if(isNumber(item) && !item.includes("°")){
         // als het niet in dms formaat is en als het een getal is dan is hij gewoon goed.
-        // newDataArray.push(item);
         return item;
       }
   }).filter(item => {return item != null})
-  console.log(newData);
+
+  return newData;
 }
 //Deze functie zet het dms formaat om naar een decimal formaat
 function convertDMS(degree, minute, second, direction){
